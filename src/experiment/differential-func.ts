ea08ee49c3a044ed49929f9601a9b96229027ccb
nayounsang <nayounsang722@gmail.com>
2024-08-27 21:09:14 +0900

    fix: change diff func

--- src/experiment/differential-func.ts
+++ src/experiment/differential-func.ts
@@ -1,50 +1,54 @@
-export const differentialObject = (prev: unknown, next: unknown): unknown => {
-	if (typeof prev !== 'object' || prev === null) {
-		return prev === next ? undefined : next
-	}
+type argType = unknown;
 
-	if (Array.isArray(prev) && Array.isArray(next)) {
-		if (prev.length === next.length) {
-			const diff = []
-			for (let i = 0; i < prev.length; i++) {
-				const value = differentialObject(prev[i], next[i])
-				if (value !== undefined) {
-					diff.push(differentialObject(prev[i], next[i]))
-				}
-			}
-			return diff.length > 0 ? diff : undefined
-		} else {
-			return next.slice(prev.length)
-		}
-	}
+export const differentialObject = (prev: argType, next: argType): argType => {
+  // 원시값인 경우 그대로
+  if (typeof prev !== "object" || prev === null) {
+    return prev === next ? undefined : next;
+  }
+  // 배열인 경우
+  if (Array.isArray(prev) && Array.isArray(next)) {
+    // key: 배열 인덱스, value: 배열 엘리먼트
+    const diff: { [key in number]: unknown } = {};
+    for (let i = 0; i < prev.length; i++) {
+      const value = differentialObject(prev[i], next[i]);
+      // 변화된 것이 있는 경우 추가
+      if (value !== undefined) {
+        diff[i] = value;
+      }
+    }
+    for (let i = prev.length; i < next.length; i++) {
+      //
+      diff[i] = next[i];
+    }
+    return Object.keys(diff).length > 0 ? diff : undefined;
+  }
 
-	// 객체일 경우 객체의 차이를 반환
-	if (typeof prev === 'object' && typeof next === 'object') {
-		const difference: { [key in string | number]: unknown } = {}
-		for (const key in next) {
-			if (next.hasOwnProperty(key)) {
-				if (!(key in prev)) {
-					difference[key] = next[key as keyof typeof next] // 새로운 key
-				} else {
-					const diff = differentialObject(
-						prev[key as keyof typeof prev],
-						next[key as keyof typeof next],
-					)
-					if (typeof diff !== 'undefined') {
-						difference[key] = diff // 차이가 존재하는 경우 갱신
-					}
-				}
-			}
-		}
-		// undefined는 차이 없음을 의미
-		// key가 존재해야 차이가 존재
-		return Object.keys(difference).length > 0 ? difference : undefined
-	}
+  // 객체일 경우 객체의 차이를 반환
+  if (typeof prev === "object" && typeof next === "object") {
+    const difference: { [key in string | number]: unknown } = {};
+    for (const key in next) {
+      if (next.hasOwnProperty(key)) {
+        if (!(key in prev)) {
+          difference[key] = next[key as keyof typeof next]; // 새로운 key
+        } else {
+          const diff = differentialObject(
+            prev[key as keyof typeof prev],
+            next[key as keyof typeof next]
+          );
+          if (typeof diff !== "undefined") {
+            difference[key] = diff; // 차이가 존재하는 경우 갱신
+          }
+        }
+      }
+    }
+    // undefined는 차이 없음을 의미
+    // key가 존재해야 차이가 존재
+    return Object.keys(difference).length > 0 ? difference : undefined;
+  }
 
-	// undefined는 차이 없음을 의미
-	return undefined
-}
-
+  // undefined는 차이 없음을 의미
+  return undefined;
+};
 
 export const differentialFunc = (arr: unknown[][]) => {
   return arr.map((e) => {
